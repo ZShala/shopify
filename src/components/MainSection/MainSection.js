d68eaf45034b71bd6a0901200d16ad85c49c1149
Zana Shala <zanashala2015@gmail.com>
2023-04-17 21:26:06 +0200

    main section added

---
+++ src/components/MainSection/MainSection.js
@@ -0,0 +1,14 @@
+import "./MainSection.css";
+import MainImg from "./main-img.png";
+import {BsArrowUpRight} from "react-icons/bs";
+
+const MainSection = () => {
+    return(
+        <div className="main-container">
+            <button className="main-btn">Shopify Editions | Winter ’23: Explore 100+ product updates  <BsArrowUpRight className="up-arrow"/></button>
+            <img src={MainImg} alt=""/>
+        </div>
+    )
+}
+
+export default MainSection;
