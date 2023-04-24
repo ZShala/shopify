9efc707f952c1a40e8825789d9ef6b734bd97c50
Zana Shala <zanashala2015@gmail.com>
2023-04-24 20:17:48 +0200

    last changes

--- src/components/MainSection/MainSection.js
+++ src/components/MainSection/MainSection.js
@@ -5,8 +5,12 @@ import {BsArrowUpRight} from "react-icons/bs";
 const MainSection = () => {
     return(
         <div className="main-container">
+            <div className="main-left-side">
             <button className="main-btn">Shopify Editions | Winter ’23: Explore 100+ product updates  <BsArrowUpRight className="up-arrow"/></button>
+            <h1>Build any commerce experience</h1>
+            </div>
             <img src={MainImg} alt=""/>
+           
         </div>
     )
 }
