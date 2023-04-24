8ff542eb0aa3d07cbb3660b983423992c881d6eb
Zana Shala <zanashala2015@gmail.com>
2023-04-24 21:27:37 +0200

    last changes

--- src/components/MainSection/MainSection.js
+++ src/components/MainSection/MainSection.js
@@ -1,18 +1,31 @@
 import "./MainSection.css";
 import MainImg from "./main-img.png";
-import {BsArrowUpRight} from "react-icons/bs";
+import LogoImg from "./img.png";
+import { BsArrowUpRight } from "react-icons/bs";
 
 const MainSection = () => {
-    return(
-        <div className="main-container">
-            <div className="main-left-side">
-            <button className="main-btn">Shopify Editions | Winter ’23: Explore 100+ product updates  <BsArrowUpRight className="up-arrow"/></button>
-            <h1>Build any commerce experience</h1>
-            </div>
-            <img src={MainImg} alt=""/>
-           
-        </div>
-    )
-}
+  return (
+    <div className="main-container">
+      <div className="main-left-side">
+        <button className="main-btn">
+          Shopify Editions | Winter ’23: Explore 100+ product updates{" "}
+          <BsArrowUpRight className="up-arrow" />
+        </button>
+        <h1>Build any commerce experience</h1>
+        <hr className="main-hr" />
+        <p className="main-p">
+          Compose, customize, and extend every part of the commerce stack—from
+          storefront to checkout to backend integrations—and create unique
+          experiences for your brand or millions of merchants around the world.
+        </p>
+        <a href="#" className="main-a">Explore docs &rarr;</a>
+      </div>
+      <img src={MainImg} alt="" />
+      <div className="main-logos">
+          <img src={LogoImg} alt=""/>
+      </div>
+    </div>
+  );
+};
 
 export default MainSection;
